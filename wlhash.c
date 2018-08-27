623470a6153ee71eff10f5f344ed576536d88358
t-sin <shinichi.tanaka45@gmail.com>
2018-08-27 15:09:18 +0900

    Initialize allocated memory

--- wlhash.c
+++ wlhash.c
@@ -10,6 +10,8 @@ int hash(WlObject* o, int max_num) {
   return o->id % max_num;
 }
 
+#define table_size(t) ((int)pow(2, t->d))
+
 WlObjectTable* make_table() {
   WlObjectTable* table;
   void* mem;
@@ -24,16 +26,18 @@ WlObjectTable* make_table() {
   table->item_count = 0;
   table->not_null_count = 0;
 
-  mem = malloc(sizeof(WlTableKey) * pow(2.0, table->d));
+  mem = malloc(sizeof(WlTableKey) * table_size(table));
   if (mem == NULL) {
     return NULL;
   }
+  memset(mem, 0, sizeof(WlTableKey) * table_size(table));
   table->keys = (WlTableKey**)mem;
 
-  mem = malloc(sizeof(WlObject) * pow(2.0, table->d));
+  mem = malloc(sizeof(WlObject) * table_size(table));
   if (mem == NULL) {
     return NULL;
   }
+  memset(mem, 0, sizeof(WlTableKey) * table_size(table));
   table->values = (WlObject**)mem;
 
   return table;
