2e30c0a2130cac3bb926ac8c582569356a8debfe
t-sin <shinichi.tanaka45@gmail.com>
2018-08-28 13:47:58 +0900

    Add some LOADing operations

--- wlopcode.h
+++ wlopcode.h
@@ -21,11 +21,13 @@ typedef enum {
 /*-----------------------------------------------------------------------*/
 /* name       args  description                                          */
 /*-----------------------------------------------------------------------*/
-OP_LOAD,/*    A     S: (stop...) -> (A (stop...))                        */
-OP_LOADNIL,/*       S: (stop...) -> (nil (stop...))                      */
-OP_LOADT,/*         S: (stop...) -> (t (stop...))                        */
+OP_LOAD,/*    A     S: (stk...) -> (A (stk...))                          */
+OP_LOADI,/*   A     S: (stk...) -> (A (stk...))                          */
+OP_LOADCH,/*  A     S: (stk...) -> (A (stk...))                          */
+OP_LOADNIL,/*       S: (stk...) -> (nil (stk...))                        */
+OP_LOADT,/*         S: (stk...) -> (t (stk...))                          */
 /*                                                                       */
-OP_CONS,/*          S: (A (B (stop...))) -> ((A . B) (stop...))          */
+OP_CONS,/*          S: (A (B (stk...))) -> ((A . B) (stk...))            */
 OP_CAR,/*           S: ((A . B) (stk...)) -> (A (stk...))                */
 OP_CDR,/*           S: ((A . B) (stk...)) -> (B (stk...))                */
 OP_EQ,/*            S: (A (B (stk...))) -> (t/nil (stk...))              */
@@ -34,6 +36,6 @@ OP_ATOM,/*          S: (A (stk...)) -> (t/nil (stk...))                  */
 OP_IF,/*            S: (A (stk...)) -> (stk...)                          */
 /*                  C: [C1, (C2 (C3 (C4...)))] -> [C2, (C3 (C4...))]     */
 /*                                             -> [C3, (C4...)]          */
-}
+} WlOpcode;
 
 #endif
