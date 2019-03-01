2907f68d4cdad290f508b500a93a8806191e846b
t-sin <shinichi.tanaka45@gmail.com>
2019-03-01 20:30:04 +0900

    Stack

--- wl_kernel.c
+++ wl_kernel.c
@@ -1,5 +1,61 @@
-#include "wl_kernel.h"
+#include<stdio.h>
+#include"wl_kernel.h"
+
+void print_obj(const WlObject* o) {
+  if (o == NULL) {
+    printf("NULL");
+    return;
+  }
+  switch (o->type) {
+    case WL_TYPE_INT:
+      printf("%d", o->num);
+      break;
+    case WL_TYPE_ARRAY:
+      printf("array not implemented");
+      break;
+    case WL_TYPE_SYMBOL:
+      printf("%s", o->name);
+      break;
+    case WL_TYPE_CONS:
+      printf("cons not implemented");
+      break;
+    default:
+      printf("unknown object");
+  }
+}
+
+void print_stack(const WlStack* s) {
+  printf("[");
+  for (int i=0; i<=s->top; i++) {
+    print_obj(s->stack[i]);
+    if (i != s->top) {
+      printf(" ");
+    }
+  }
+  printf("]");
+}
 
 int main(void) {
+  WlObject o1, o2;
+  WlStack* s;
+  o1.type = WL_TYPE_INT;
+  o1.num = 1;
+  o2.type = WL_TYPE_INT;
+  o2.num = 2;
+
+  s = wl_init_stack(10);
+  print_stack(s); printf("\n");
+  wl_stack_push(s, &o1);
+  print_stack(s); printf("\n");
+  wl_stack_push(s, &o2);
+  printf("poped: "); print_obj(wl_stack_pop(s)); printf("\n");
+  printf("peeked: "); print_obj(wl_stack_peek(s)); printf("\n");
+  printf("poped: "); print_obj(wl_stack_pop(s)); printf("\n");
+  print_stack(s); printf("\n");
+  printf("poped: "); print_obj(wl_stack_pop(s)); printf("\n");
+  printf("poped: "); print_obj(wl_stack_pop(s)); printf("\n");
+  print_stack(s); printf("\n");
+  printf("poped: "); print_obj(wl_stack_pop(s)); printf("\n");
+
   return 0;
 }
--- wl_kernel.h
+++ wl_kernel.h
@@ -3,9 +3,15 @@
 //   - threaded code
 //   - stack based
 //
-#ifdef wl_kernel_h
+#ifndef wl_kernel_h
 #define wl_kernel_h
 
+#include<stdlib.h>
+
+#define WL_TRUE 1
+#define WL_FALSE 0
+
+// wl object
 typedef enum {
   WL_TYPE_INT,
   WL_TYPE_ARRAY,
@@ -26,27 +32,67 @@ typedef struct WlObject {
     };
     char* name;     // symbol
     struct {        // cons
-      WlObject* car;
-      WlObject* cdr;
+      struct WlObject* car;
+      struct WlObject* cdr;
     };
     struct {        // proc
       int p_size;
       struct WlObject** program;
     };
-  }
+  };
 } WlObject;
 
+// stack
 typedef struct WlStack {
   int top;
+  int size;
   WlObject** stack;
 } WlStack;
 
-int wl_stack_push(WlStack* s, const WlObject* o);
-WlObject* wl_stack_pop(WlStack* s);
-WlObject* wl_stack_peek(WlStack* s);
-int wl_stack_full_p(WlStack* s);
-void wl_stack_resize(WlStack* s);
+WlStack* wl_init_stack(int size) {
+  WlStack* stack = (WlStack*)malloc(sizeof(WlStack));
+  stack->size = size;
+  stack->top = -1;
+  stack->stack = (WlObject**)malloc(sizeof(WlObject) * size);
+  return stack;
+}
 
+int wl_stack_full_p(WlStack* s) {
+  return ((s->top + 1) >= s->size) ? WL_FALSE : WL_TRUE;
+}
+
+void wl_stack_resize(WlStack* s, int new_size) {
+  WlObject** new_stack = (WlObject**)malloc(sizeof(WlObject) * new_size);
+  for (int i=0; i<=s->top; i++) {
+    new_stack[i] = s->stack[i];
+  }
+  free(s->stack);
+  s->size = new_size;
+  s->stack = new_stack;
+}
+
+void wl_stack_push(WlStack* s, WlObject* o) {
+  if (wl_stack_full_p(s)) {
+    wl_stack_resize(s, s->size + 1000);
+  }
+  s->top++;
+  s->stack[s->top] = o;
+}
+
+WlObject* wl_stack_pop(WlStack* s) {
+  if (s->top == -1) {
+    return NULL;
+  }
+  return s->stack[s->top--];
+}
+WlObject* wl_stack_peek(WlStack* s) {
+  if (s->top == -1) {
+    return NULL;
+  }
+  return s->stack[s->top];
+}
+
+// VM
 typedef struct WlKernelVM {
   unsigned int pc;
   WlObject* program;
